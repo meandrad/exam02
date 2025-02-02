3e97e5aead1c90c0b6728bc97e351e55c7b5c6a7
Melissa Andrade Garcia Da Silva <meandrad@c1r5p1.42sp.org.br>
2025-02-01 21:01:40 -0300

    search_and_replace resolution

---
+++ Level 1/search_and_replace/search_and_replace.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+char	*ft_search(char *str, char *a, char *b)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == *a)
+		{
+			str[i] = *b;
+			write(1, &str[i], 1);
+		}
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
+	return (str);
+}
+
+int main (int argc, char *argv[])
+{
+	if (argc == 4)
+	{
+		if ((ft_strlen(argv[2]) > 1) || (ft_strlen(argv[3]) > 1))
+		{
+			write(1, "\n", 1);
+			return (0);
+		}
+		else
+			ft_search(argv[1], argv[2], argv[3]);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
