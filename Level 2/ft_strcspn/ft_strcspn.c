582db980c33136ebed44b52c26541297cce9114c
Melissa Andrade Garcia Da Silva <meandrad@c1r4p1.42sp.org.br>
2025-02-03 22:15:01 -0300

    ft_strcspn.c

--- Level 2/ft_strcspn/ft_strcspn.c
+++ Level 2/ft_strcspn/ft_strcspn.c
@@ -1,14 +1,5 @@
 #include <stdlib.h>
 
-static int	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
 
 size_t	ft_strcspn(const char *s, const char *reject)
 {
@@ -27,5 +18,5 @@ size_t	ft_strcspn(const char *s, const char *reject)
 		}
 		j++;
 	}
-	return (ft_strlen(s));
+	return (i);
 }
