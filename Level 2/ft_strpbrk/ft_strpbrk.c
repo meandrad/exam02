8f70cb6ac59638054451ff9be0df456c8b14a925
Melissa Andrade Garcia Da Silva <meandrad@c1r8p1.42sp.org.br>
2025-02-05 19:48:42 -0300

    ft_strpbrk resolution

---
+++ Level 2/ft_strpbrk/ft_strpbrk.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+
+char	*ft_strpbrk(const char *s1, const char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		j = 0;
+		while (s2[j] != '\0')
+		{
+			if (s2[j] == s1[i])
+				return ((char *)&s1[i]);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
