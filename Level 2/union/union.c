af5817773ff6d0569819cd0a26c44e952538d4aa
Melissa Andrade Garcia Da Silva <meandrad@c1r8p3.42sp.org.br>
2025-02-10 21:34:04 -0300

    union resolution

---
+++ Level 2/union/union.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_union(char *s1, char *s2)
+{
+	int	i;
+	int	lookup[256] = {0};
+	
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		if (lookup[(int)s1[i]] == 0)
+		{
+			lookup[(int)s1[i]] = 1;
+				ft_putchar(s1[i]);
+		}
+		i++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		if (lookup[(int)s2[i]] == 0)
+		{
+			lookup[(int)s2[i]] = 1;
+			ft_putchar(s2[i]);
+		}
+		i++;
+	}
+}
+
+int main (int argc, char *argv[])
+{
+	if (argc == 3)
+		ft_union(argv[1], argv[2]);
+	ft_putchar('\n');
+	return (0);
+}
