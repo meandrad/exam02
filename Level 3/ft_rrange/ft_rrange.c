59036210482fc3226068df75b5db2606e63eaf9d
Melissa Andrade Garcia Da Silva <meandrad@c3r9p2.42sp.org.br>
2025-02-15 16:37:54 -0300

    ft_rrange resolution

---
+++ Level 3/ft_rrange/ft_rrange.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+int	*ft_rrange(int start, int end)
+{
+	int	i;
+	int	size;
+	int	*array;
+
+	if (start <= end)
+		size = end - start + 1;
+	else
+		size = start - end + 1;
+	array = (int *)malloc(sizeof(int) * size);
+	if (!array)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		if (start <= end)
+			array[i] = end - i;
+		else
+			array[i] = end + i;
+		i++;
+	}
+	return (array);
+}
+
