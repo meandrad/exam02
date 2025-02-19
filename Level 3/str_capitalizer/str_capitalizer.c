7a7cd17ba9a0034d3dc7947e947b41f111094fbc
macmac <macmac@MacBook-Pro-de-macmac.local>
2025-02-19 19:43:00 -0300

    str_capitalizer.c resolution

---
+++ Level 3/str_capitalizer/str_capitalizer.c
@@ -0,0 +1,63 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+char	*to_lower(char *str)
+{
+	int i ;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 65 && str[i] <= 90)
+			str[i] = str[i] + 32;
+		i++;
+	}
+	return (str);
+}
+
+void	str_capitalizer(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{	
+		if (str[0] >= 97 && str[0] <= 122)
+		{
+			str[0] = str[0] - 32;
+			ft_putchar(str[i]);
+			i++;
+		}
+		if (str[i] >= 97 && str[i] <= 122 && str[i - 1] == 32 )
+		{
+			str[i] = str[i] - 32;
+			ft_putchar(str[i]);
+		}
+		else
+			ft_putchar(str[i]);
+		i++;
+	}
+	ft_putchar('\n');
+}
+int main (int argc, char *argv[])
+{
+	int	i;
+	char	*str;
+
+	i = 1;
+	if (argc != 1)
+	{
+		while (argv[i] != NULL)
+		{
+			str = to_lower(argv[i]);
+			str_capitalizer(str);
+			i++;
+		}
+	}
+	else
+		ft_putchar('\n');
+	return (0);
+}
