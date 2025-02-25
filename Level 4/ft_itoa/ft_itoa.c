8de5ce145c2339e4b869b8078027a0896cf92a0b
Melissa Andrade Garcia Da Silva <meandrad@c3r11p3.42sp.org.br>
2025-02-24 21:20:05 -0300

    ft_itoa resolution

--- Level 4/ft_itoa/ft_itoa.c
+++ Level 4/ft_itoa/ft_itoa.c
@@ -25,26 +25,26 @@ int	count_number(int nbr)
 
 char *ft_itoa (int nbr)
 {
-	char	*str;
+	char	*s;
 	long	num;
 	int		length;
 
 	length = count_number(nbr);
-	str = (char *)malloc(sizeof(char) * length + 1);
-	if (!str)
+	s = (char *)malloc(sizeof(char) * length + 1);
+	if (!s)
 		return (NULL);
 	num = nbr;
 	if (num < 0)
 	{
-		str[0] = '-';
+		s[0] = '-';
 		num *= -1;
 	}
 	while (num > 0)
 	{
 		length--;
-		if (str[length] != '-')
-			str[length] = (num % 10) + '0';
+		if (s[length] != '-')
+			s[length] = (num % 10) + '0';
 		num /= 10;
 	}
-	return (str);
+	return (s);
 }
