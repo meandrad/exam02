5abe13e0da0f75d16d7633fb1cc97ad36483f842
Melissa Andrade Garcia Da Silva <meandrad@c1r6p1.42sp.org.br>
2025-03-04 17:12:07 -0300

    ft_strcspn resolution

--- Level 2/ft_strcspn/ft_strcspn.c
+++ Level 2/ft_strcspn/ft_strcspn.c
@@ -6,10 +6,10 @@ size_t	ft_strcspn(const char *s, const char *reject)
 	int	j;
 
 	j = 0;
-	while(reject[j] != '\0')
+	while(s[i] != '\0')
 	{
 		i = 0;
-		while (s[i] != '\0')
+		while (reject[i] != '\0')
 		{
 			if (s[i] == reject[j])
 				return (i);
